9afab4bc026808363714e1c7aefc2a0cf7eeaa09
Mario César Señoranis Ayala <mariocesar.c50@gmail.com>
2017-03-01 13:14:16 -0400

    Update packages on every boot

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,8 +4,20 @@ set -eo pipefail
 # Fix permissions if needed.
 find /app ! -user $APP_USER -exec chown $APP_USER:$APP_USER {} \;
 
+update-node-env() {
+    if [ -f /app/package.json ]; then
+    echo -n "* Installing packages"
+    gosu app npm install /app/package.json
+    echo "[Done]"
+    fi
+}
+
+export -f update-node-env
+
 case "$1" in
     npm|gulp|webpack|-)
+        update-node-env
+
         # Cleanup shortcut
         if [ ${1} = '-' ]; then
             shift
